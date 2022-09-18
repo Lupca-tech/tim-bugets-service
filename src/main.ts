687a157c52ee213bede98dec45a5c0ef58067202
tungdt-3023 <dang.thanh.tung@sun-asterisk.com>
2022-09-18 21:52:06 +0700

    init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 3001
+      }      
+    }
+  );
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen();
 }
 bootstrap();
